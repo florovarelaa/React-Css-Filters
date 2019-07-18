23d50961d4d172482c63c3737069ab03dbe7b67a
florovarelaa <florovarelaa@gmail.com>
2019-07-17 23:17:54 -0300

    Updates

---
+++ precache-manifest.cdc5ea65200a862cef2785ca5708fe73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5f0f9273303e4f12a43826d584a24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e67e080e3367e1ce41ba",
+    "url": "/static/css/main.3fcbdd0b.chunk.css"
+  },
+  {
+    "revision": "2e0172335212d05e24eb",
+    "url": "/static/js/2.8ddd6bba.chunk.js"
+  },
+  {
+    "revision": "e67e080e3367e1ce41ba",
+    "url": "/static/js/main.e19acda8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1fbe0a3c5b3c28d46010527a4d86f466",
+    "url": "/static/media/leopard.1fbe0a3c.jpg"
+  }
+]);
